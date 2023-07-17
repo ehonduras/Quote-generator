4a484d9dca4512316ea7a750e47005318b00d5f2
ldrljaca <ermin.h@protonmail.com>
2023-07-17 13:02:50 +0200

    te

--- script.js
+++ script.js
@@ -1,80 +1,94 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
-const form = document.getElementById('form');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+const form = document.getElementById("form");
 
 let apiQuotes = [];
 let userAuthenticated = false;
 
-function validateLogin (e){
-    e.preventDefault();
-    let username = document.getElementById('username');
-    let password = document.getElementById('password');
+function validateLogin(e) {
+  e.preventDefault();
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
 
-    pe.setPerson({ phone: '38761545933'}, 2000 ).then(() => {
-        return pe.track('login');
-    });
+  pe.setPerson({ phone: "38761545933" }, 2000).then(() => {
+    return pe.track("login");
+  });
 
-    let user = {
-        username: username.value,
-        password: password.value,
-    };
+  let user = {
+    username: username.value,
+    password: password.value,
+  };
 
-    localStorage.setItem(user, JSON.stringify(user));
-    console.log('user added');
+  localStorage.setItem(user, JSON.stringify(user));
+  console.log("user added");
 }
 
-function loading(){
-    loader.hidden = false;
-    quoteContainer.hidden = true;
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
-function complete(){
-    quoteContainer.hidden = false;
-    loader.hidden = true;
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
 }
 
-function newQuote(){
-    loading();
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    
-    if(!quote.author){
-        authorText.textContent = 'Unknown';
-    } else{
-        authorText.textContent = quote.author;
-    }
+function newQuote() {
+  loading();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
-    if(quote.text.length > 120){
-        quoteText.classList.add('long-quote');
-    } else{
-        quoteText.classList.remove('long-quote');
-    }
-    quoteText.textContent = quote.text;
-    complete();
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+  complete();
 }
 
-async function getQuotes(){
-    loading();
-    const apiUrl = 'https://type.fit/api/quotes';
-    try{
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        newQuote();
-    } catch(error){
-        //catch error here
-    }
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //catch error here
+  }
 }
 
-function tweetQuote(){
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterUrl, '_blank');
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
 }
 
-newQuoteBtn.addEventListener('click', newQuote);
-twitterBtn.addEventListener('click', tweetQuote);
-form.addEventListener('submit', validateLogin);
+infobipWebPush("init", {
+  applicationCode: "applicationCode",
+  vapidPublicKey: "vapidPublicKey from Infobip portal",
+  safariWebsitePushId: "safariWebsitePushId from apple Developer portal",
+});
+
+infobipWebPush("on", "init", function (sdk) {
+  sdk.saveCustomAttributes({
+    customStringAttribute: "testiranje",
+    customNumberAttribute: 42,
+    customBooleanFlag: true,
+  });
+});
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+form.addEventListener("submit", validateLogin);
 
 getQuotes();
